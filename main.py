247a56e45c52e354b517b56aea0c951baa98dfea
adarshagnihotri2007 <adarshagnihotri2007@gmail.com>
2026-06-28 19:09:02 +0530

    Added exception handling to NewsPulse

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ for source_name in RSS_FEEDS:
 source = input("Enter Source: ").lower()
 
 if source in RSS_FEEDS:
-    news = fetch_news(source)
 
-    for article in news:
-        print("Title:", article["title"])
-        print("Published:", article["published"])
-        print("Link:", article["link"])
-        print("-" * 80)
+    try:
+        news = fetch_news(source)
+
+        for article in news:
+            print("Title:", article["title"])
+            print("Published:", article["published"])
+            print("Link:", article["link"])
+            print("-" * 80)
+
+    except Exception as e:
+        print("Unable to fetch news!")
+        print(f"Error: {e}")
 
 else:
     print("Invalid Source!")
