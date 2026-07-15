8047ffe86c73e50127bda422a0fdd87cf69ed281
adarshagnihotri2007 <adarshagnihotri2007@gmail.com>
2026-07-15 12:01:57 +0530

    Replace CNN with Guardian RSS and improve news data quality

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 RSS_FEEDS = {
     "bbc": "https://feeds.bbci.co.uk/news/rss.xml",
-    "cnn": "http://rss.cnn.com/rss/edition.rss",
+    "guardian": "https://www.theguardian.com/world/rss",
     "toi": "https://timesofindia.indiatimes.com/rssfeedstopstories.cms"
-} 
+}
